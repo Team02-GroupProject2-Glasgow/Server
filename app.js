ba4ea3becaf62e61a4830b45a87900c0b055519b
AnthonyGunardi <anthonygunardi@gmail.com>
2021-01-22 00:51:20 +0700

    add more socket on app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const cors = require("cors")
-const { Socket } = require('dgram')
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app);
@@ -8,7 +7,13 @@ const io = require('socket.io')(http)
 const { shuffleWord, getRandom } = require('./word')
 
 let score = {
+
 }
+
+let correct = []
+let counter = 0
+const submitted = new Set([])
+
 
 app.use(cors())
 
@@ -20,6 +25,73 @@ let shuffle = ''
 io.on('connect', (socket) => {
   console.log('a user connected')
   socket.emit('init', score)
+
+  socket.on('newPlayer', function(player) {
+    const numPlayers = Object.keys(score).length
+    if(numPlayers === 2){
+      io.to(socket.id).emit('roomExceeded', 'I just met you');
+      return
+    }
+    score[player] = 0
+    io.emit('serverPlayers', score)
+  })
+  
+  socket.on('getQuestion', function () {
+    currentWord = parse[getRandom(1, 9)].word.toLowerCase() //jawaban
+    shuffle = shuffleWord(currentWord) //hasil shuffle
+    let payload = { currentWord, shuffle, score }
+    // console.log(payload, '<<<<<<<<<<<<di app.js')
+    counter++
+    io.emit('newQuestion', payload)
+  })
+
+  socket.on('checkAnswer', function (payload) {  
+    if(submitted.has(payload.player)){
+      return
+    }
+    submitted.add(payload.player)
+
+    if (payload.answer == currentWord ) {
+      score[payload.player] += 10
+      correct.push(true)
+    } else {
+      score[payload.player] += 0
+      correct.push(false) 
+    }
+
+    if(counter === 6){  //jumlah round untuk finish game
+      const [playerA, playerB] = Object.keys(score)
+      if(score[playerA] === score[playerB]){
+        io.emit('gameOverWithDraw', score)
+        return
+      }     
+      const winner = score[playerA] > score[playerB] ? playerA : playerB
+      counter = 0
+      io.emit('gameOverWithWinner', {...score, winner})
+      return
+    }
+
+    if(correct.some(e => e === true) || correct.length === 2){
+      currentWord = parse[getRandom(1, 2)].word.toLowerCase() //jawaban
+      shuffle = shuffleWord(currentWord) //hasil shufflean
+
+      let data = { currentWord, shuffle }
+      correct = []
+      submitted.clear()
+      io.emit('newQuestion', { ...data, score})
+      counter++
+      return 
+    }
+
+    io.emit('serverPlayers', score)
+  })
+
+  socket.on("exit", () => {
+    correct = []
+    submitted.clear()
+    score = {}
+    io.emit('exit')
+  })
 })
 
 http.listen(3000, () => {
