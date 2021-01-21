5cdcba581a7d3a046cbeb5771db9c051737a1449
AnthonyGunardi <anthonygunardi@gmail.com>
2021-01-21 22:57:16 +0700

    write app.js, add word.js & wordlist

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+const fs = require('fs')
+const cors = require("cors")
 const { Socket } = require('dgram')
 const express = require('express')
 const app = express()
-const http = require('http').Server(app)
+const http = require('http').createServer(app);
 const io = require('socket.io')(http)
+const { shuffleWord, getRandom } = require('./word')
 
-io.on('connection', (socket) => {
-    console.log('Connected')
+let score = {
+}
+
+app.use(cors())
+
+let data = fs.readFileSync('./wordlist.json', 'UTF-8')
+let parse = JSON.parse(data)
+let currentWord = ''
+let shuffle = ''
+
+io.on('connect', (socket) => {
+  console.log('a user connected')
+  socket.emit('init', score)
 })
 
 http.listen(3000, () => {
