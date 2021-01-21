a00a6411054144ed7756a69c9cbe6e48a7894777
abbymakarim <abby.makarim@gmail.com>
2021-01-21 20:48:44 +0700

    changed const http to server, added kata as database

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const { Socket } = require('dgram')
 const express = require('express')
 const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+const tebak_kata = [
+  {
+    id: 1,
+    clue: ['kendaraan', 'roda empat', 'bensin', 'darat', 'supir'],
+    answer: 'mobil'
+  },
+  {
+    id: 2,
+    clue: ['istirahat', 'capek', 'menunggu', 'perabotan', 'kaki empat'],
+    answer: 'kursi'
+  },
+  {
+    id: 3,
+    clue: ['tipis', 'persegi panjang', 'ringan', 'permainan', 'joker'],
+    answer: 'kartu'
+  },
+  {
+    id: 4,
+    clue: ['bulat', 'makanan', 'daging', 'sapi', 'ikan'],
+    answer: 'bakso'
+  },
+  {
+    id: 5,
+    clue: ['penampilan', 'cowok', 'muka', 'ganteng', 'cewek juga bisa'],
+    answer: 'cakep'
+  }
+]
 
 io.on('connection', (socket) => {
-    console.log('Connected')
+    console.log('Socket.io client connected')
 })
 
-http.listen(3000, () => {
-    console.log('listening on *:3000')
+server.listen(3000, () => {
+    console.log('Listening to port ' + 3000)
 })
