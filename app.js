9533508505fadc19377c25a14b4c132cecb8d6e4
pajarwitarsa <pajarwitarsa@gmail.com>
2021-01-22 14:14:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const PORT = 3000 || process.env.PORT
 
 let user = []
 
@@ -93,8 +94,6 @@ function userLeave(id){
       return user.splice(index, 1)
   }
 }
-
-const PORT = 3000 || process.env.PORT
 
 server.listen(PORT, () => {
     console.log('Listening to port ' + PORT)
